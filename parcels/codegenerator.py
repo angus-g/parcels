940c5203204132e47936b040de4fb71c0cf35f82
Angus Gibson <angus@agibson.me>
2019-07-27 09:36:38 +1000

    Run kernel particle loop with openMP

--- parcels/codegenerator.py
+++ parcels/codegenerator.py
@@ -933,7 +933,10 @@ class LoopGenerator(object):
         fbody = c.Block([c.Value("int", "p, sign_dt, sign_end_part"), c.Value("ErrorCode", "res"),
                          c.Value("int", "repeat"),
                          c.Value("double", "__dt, __tol"), c.Assign("__tol", "1.e-6"),
-                         sign_dt, init_repeat, particle_backup, part_loop, c.Statement("return repeat")])
+                         c.Statement("omp_set_num_threads(omp_get_num_procs())"),
+                         sign_dt, init_repeat, particle_backup,
+                         c.Pragma("omp parallel for private(p, sign_end_part, __dt, res, particle_backup) reduction(|:repeat)"),
+                         part_loop, c.Statement("return repeat")])
         fdecl = c.FunctionDeclaration(c.Value("int", "particle_loop"), args)
         ccode += [str(c.FunctionBody(fdecl, fbody))]
         return "\n\n".join(ccode)
